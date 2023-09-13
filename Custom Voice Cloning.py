cad9ad33d9766ad5ee755f79df69ba8d57a472fb
Prabhuraj Dhondge <141675311+PrabhurajSD@users.noreply.github.com>
2023-09-14 00:05:25 +0530

    Update Custom Voice Cloning.py

--- Custom Voice Cloning.py
+++ Custom Voice Cloning.py
@@ -3,7 +3,6 @@
 !pip3 install -U scipy
 
 !git clone https://github.com/jnordberg/tortoise-tts.git
-# %cd tortoise-tts
 !pip3 install -r requirements.txt
 !pip3 install transformers==4.19.0 einops==0.5.0 rotary_embedding_torch==0.1.5 unidecode==1.3.5
 !python3 setup.py install
